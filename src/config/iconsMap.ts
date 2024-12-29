e22755b1917330ef626ee78f19db49b5083aa3f9
fm-anderson <78233612+fm-anderson@users.noreply.github.com>
2024-12-29 12:23:28 -0500

    types

---
+++ src/config/iconsMap.ts
@@ -0,0 +1,15 @@
+import {
+  HomeIcon,
+  AcademicCapIcon,
+  PuzzlePieceIcon,
+  SpeakerWaveIcon,
+} from "@heroicons/react/24/solid";
+
+const ICONS_MAP = {
+  HomeIcon,
+  AcademicCapIcon,
+  PuzzlePieceIcon,
+  SpeakerWaveIcon,
+};
+
+export default ICONS_MAP;
